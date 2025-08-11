4b457e94529c439ccdd1400a1b1ef93ec687d8a9
Sammy <myallio94@gmail.com>
2025-08-11 13:18:08 +1000

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,7 @@ Helper function
 
 
 def index_range(page, page_size):
+    '''returns a tuple of start index adn end index'''
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
